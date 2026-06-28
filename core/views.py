a9f877ec3e6c36dbffdf2859ac5d0c5c1d6836eb
Anderson Felipe Dias Domingos <139730074+Felipe-dmgs@users.noreply.github.com>
2026-06-27 22:58:52 -0300

    feat/apostila5

--- core/views.py
+++ core/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tarefa
+from .forms import TarefaForm
 
 def home(request):
-    context = {
-        'nome_usuario':'Júnior',
-        'tecnologias':['Python', 'Django', 'HTML', 'CSS']
-    }
-    return render(request,'home.html', context)
+
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+
+    else:
+        form = TarefaForm()
+
+        todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
+
+        context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'],
+        'tarefas': todas_as_tarefas,
+        'form': form,
+        }
+        return render(request, 'home.html', context)
+
+def concluir_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa,pk=pk)
+
+    if request.method == 'POST':
+        tarefa.concluida = True
+        tarefa.save()
+
+        return redirect('home')
+
+def deletar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    
+    if request.method == 'POST':
+        tarefa.delete()
+
+        return redirect('home')
